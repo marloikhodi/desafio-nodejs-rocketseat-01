582bc3d5115ee24439c95ed2ff0c994af7d739f2
marloikhodi <deivid.serafim03@gmail.com>
2025-10-01 10:08:02 -0300

    feat(utils): requisição de rota com filtro por id

---
+++ src/utils/build-route-path.js
@@ -0,0 +1,7 @@
+export function buildRoutePath(path) {
+  const routeParametersRegex = /:([a-zA-Z]+)/g;
+  const pathWithParams = path.replaceAll(routeParametersRegex, "(? <$1>[-a-z0-9_]+");
+
+  const pathRegex = new RegExp(`^${pathWithParams}`);
+  return pathRegex;
+}
